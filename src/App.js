3413442128338413d238e1d82c989c5e0a961f08
Charlie8rown <105841366+Charlie8rown@users.noreply.github.com>
2023-02-28 10:49:56 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Showcase from "./Showcase";
+import BaseStats from "./BaseStats";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="main-wrapper background">
+      <Showcase />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/BaseStats.js
@@ -0,0 +1,7 @@
+import "./BaseStats.css";
+
+function BaseStats() {
+  return <h1>BaseStats</h1>;
+}
+
+export default BaseStats;
